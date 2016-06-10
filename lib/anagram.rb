f3d45285354093f1588cc2fe58b03a74109f0f53
Kellye Greene <zephyr-eng+kellye@gust.com>
2016-06-10 14:36:57 -0400

    Anagram complete

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
-# Your code goes here!
+require "pry"
+
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array_of_anagrams)
+    matches_array = array_of_anagrams.select {|single_word| single_word.chars.sort == @word.chars.sort }
+    if matches_array != []
+      return matches_array
+    else 
+      return []
+    end
+  end
+
+end
+
+# %w(enlists google inlets banana)
+# inlets
+#
